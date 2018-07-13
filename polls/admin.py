393bff3c93fe1a66c0efb477dc7bc3a5c106c112
cuigelasi <cuijianmin@yeah.net>
2018-07-13 16:19:29 +0800

    polls_02

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
+# encoding=utf-8
 from django.contrib import admin
-
+from .models import Question, Choice
 # Register your models here.
+
+admin.site.register(Question)
+admin.site.register(Choice)
